12a97f8130462053e492a45046680b56df1cd9c8
Debbie Urquhart <debiurquhart@live.co.uk>
2020-05-14 14:04:54 +0100

    allows user input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { APIService } from "./API.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'myAmplifyProject';
+export class AppComponent implements OnInit {
+  todos: Array<any>;
+
+  title = "myAmplifyProject";
+  constructor(private apiService: APIService) {}
+
+  createTodo(name, description) {
+    this.apiService.CreateTodo({
+      name: name,
+      description: description
+    });
+  }
+
+  async ngOnInit() {
+    this.apiService.ListTodos().then(evt => {
+      this.todos = evt.items;
+    });
+
+    this.apiService.OnCreateTodoListener.subscribe(evt => {
+      const data = (evt as any).value.data.onCreateTodo;
+      this.todos = [...this.todos, data];
+    });
+  }
 }
